416fd659830882c312c0092b5554265a88762112
Deeptanu Bhatta <deeptanubhatta2002@gmail.com>
2024-08-17 21:02:53 +0530

    another commit

--- twitter.sol
+++ twitter.sol
@@ -2,17 +2,33 @@ pragma solidity 0.8.1;
 
 contract Twitter {
 
-    mapping (address => string[]) public tweets;
-
-    function createTweets(string memory _tweets) public{
-        tweets[msg.sender].push(_tweets);
+    struct Tweet {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
     }
 
-    function getAllTweets(address _owner) public view returns (string[] memory){
+    mapping (address => Tweet[]) public tweets;
+
+    function createTweets(string memory _tweets) public{
+       
+        Tweet memory newTweet = Tweet({
+            author:msg.sender,
+            content:_tweets,
+            timestamp:block.timestamp,
+            likes:0
+
+
+        });
+        tweets[msg.sender].push(newTweet);
+    }
+
+    function getAllTweets(address _owner) public view returns (Tweet[] memory){
         return tweets[_owner];
     }
 
-    function getParticularTweets(address _owner,uint256 _i) public view returns(string memory){
+    function getParticularTweets(address _owner,uint256 _i) public view returns(Tweet memory){
         return tweets[_owner][_i];
     }
 }
